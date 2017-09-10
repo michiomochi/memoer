0a680203c136ed78fe86e5cba853bd9f73b266ad
Michikawa Masayoshi <michikawa.masayoshi@gmail.com>
2017-09-11 00:42:02 +0900

    Create memoer-editor

---
+++ lib/memoer-editor/src/MemoerEditor.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export default class MemoerEditor extends React.component<any, any> {
+
+}
---
+++ lib/memoer-editor/src/MemoerEditorContainer.tsx
@@ -0,0 +1,17 @@
+import { connect } from "react-redux";
+
+import MemoerEditor from "./MemoerEditor";
+
+function mapStateToProps(state, ownProps) {
+  return {
+
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MemoerEditor);
